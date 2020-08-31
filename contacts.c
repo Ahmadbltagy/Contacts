e78144b02c52c706e5d7cbeb36cf83d16b568ad9
Ahmad El Beltagy <elbeltagyahmad@gmail.com>
2020-08-31 02:14:41 +0200

    Add search function

--- contacts.c
+++ contacts.c
@@ -54,16 +54,38 @@ void Remove(){
 
 }
 
+int Search(){
+    s = fopen(fname, "r");
+    char name[MAX], cmp[MAX], line[MAX];
+    printf("Enter the name: ");
+    scanf("%s", name);
+    //to make first char upper
+    if(name[0] >= 97)
+        name[0] -= 32;
+    while( !feof(s) ){
+        strcpy(line, "\0");
+        fscanf(s, "%s", cmp);
+        if( strcmp(name, cmp) == 0 ){
+            fgets(line, MAX, s);
+            printf("\n%s %s", cmp, line);
+            return 0;
+        }
+        
+    }
+    printf("\nNot found\n");
+}
+
 int main(){
     while( 1 ){
         int req;
-        printf("\n\nTo add a new name press 1\n"
+        printf("\nTo add a new name press 1\n"
                "To show contacts press 2\n"
                "To remove name press 3\n"
+               "To search for a name press 4\n"
                "To Exit press 0 \n");
         scanf("%d", &req);
 
-        req == 0? exit(0) : req == 1? Add() : ( req == 2? Show() : Remove() );
+        req == 0? exit(0) : req == 1? Add() : req == 2? Show() : req == 3? Remove() : req == 4? Search(): printf("\n\nWorng input");
     }
 
 }
